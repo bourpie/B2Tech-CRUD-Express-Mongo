ca1df1c178bb811f77f0e12a554f6751615863c7
pbourgeois <pbcomweb@gmail.com>
2023-01-25 15:45:13 -0500

    ménage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 require('./db/mongoose');
 const express = require('express');
 const app = express();
-const Blog = require('./db/models/blog');
 const Plum = require('./db/models/plumitif');
 const Dossier = require('./db/models/dossier');
 
 app.use(express.json());
 
+// Data locale
 app.get('/plumitifs', (req, res) => {
     Plum.find({}).then(sejs => {
         res.send(sejs);
@@ -15,26 +15,10 @@ app.get('/plumitifs', (req, res) => {
     });
 })
 
+// Mock API SEJ
 app.get('/dossiers', (req, res) => {
     Dossier.find({}).then(dossiers => {
         res.send(dossiers);
-    }).catch(error => {
-        res.status(400).send(error)
-    });
-})
-
-app.get('/blogs', (req, res) => {
-    Blog.find({}).then(blogs => {
-        res.send(blogs);
-    }).catch(error => {
-        res.status(400).send(error)
-    });
-})
-
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
-    blog.save().then(blog => {
-        res.status(201).send(blog);
     }).catch(error => {
         res.status(400).send(error)
     });
